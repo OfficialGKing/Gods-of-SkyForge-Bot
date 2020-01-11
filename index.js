940f656c27987f5fb6a5c1044d96ded92fe2566c
Gregory King <webddesign101@gmail.com>
2020-01-11 22:09:35 +0000

    Adding responses

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, RichEmbed } = require('discord.js');
-
+const axios = require("axios");
 const client = new Client();
 const prefix = '?sky';
 
@@ -7,10 +7,17 @@ client.on("ready", () => {
   console.log("Ready");
   client.user.setActivity("SkyForge");
 });
-client.on("message", (message) => {
+client.on("message", async (message) => {
   if(message.author.bot) return;
   if(message.content.startsWith(prefix + "login")) {
-    
+    let get_user = async () => {
+      let response = await axios.get(`https://godsofskyforge.bubbleapps.io/version-test/api/1.1/obj
+/User?Username=${args[0]}&email=${args[1]}`)
+      let user = response.data
+      return user
+    }
+    let userValue = await get_user()
+    console.log(userValue)
   }
 });
 
